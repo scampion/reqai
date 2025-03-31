4a38dafe0c96581315b9e2613862f510fd677d81
Sebastien Campion <sebastien.campion@gmail.com>
2025-03-31 20:17:07 +0200

    feat: Serve requirements data from data.json

--- server.py
+++ server.py
@@ -12,11 +12,14 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
-            response = {
-                'message': 'Hello from the server!',
-                'data': ['item1', 'item2', 'item3']
-            }
-            self.wfile.write(json.dumps(response).encode('utf-8'))
+            with open('data.json', 'r') as file:
+                data = json.load(file)
+                requirements_data = data.get('requirements', [])
+                response = {
+                    'message': 'Requirements data from the server!',
+                    'data': requirements_data
+                }
+                self.wfile.write(json.dumps(response).encode('utf-8'))
             return
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
@@ -25,5 +28,3 @@ Handler = MyHttpRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at port {PORT}")
     httpd.serve_forever()
-
-
