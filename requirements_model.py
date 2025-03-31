55979ca6bb23a5e3d94e34815a08ef9ac39fc765
Sebastien Campion <sebastien.campion@gmail.com>
2025-03-31 19:53:16 +0200

    feat: Add deadline attribute to Goal class

--- requirements_model.py
+++ requirements_model.py
@@ -27,15 +27,16 @@ class StakeholdersModel:
         return f"StakeholdersModel(stakeholders={self.stakeholders})"
 
 class Goal:
-    def __init__(self, identifier, description, priority, stakeholder_ids):
+    def __init__(self, identifier, description, priority, stakeholder_ids, deadline):
         self.identifier = identifier
         self.description = description
         self.priority = priority
         self.stakeholder_ids = stakeholder_ids
+        self.deadline = deadline
 
     def __repr__(self):
         return (f"Goal(identifier={self.identifier}, description={self.description}, priority={self.priority}, "
-                f"stakeholder_ids={self.stakeholder_ids})")
+                f"stakeholder_ids={self.stakeholder_ids}, deadline={self.deadline})")
 
 class GoalsModel:
     def __init__(self):
